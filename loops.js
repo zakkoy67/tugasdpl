8ff58cd18eae61779fedff93c2a6d765f11ec912
Midori Harahap <118705697+midoistory@users.noreply.github.com>
2022-11-22 13:34:23 +0700

    22/11/2022

---
+++ loops.js
@@ -0,0 +1,8 @@
+//membuat output ganjil untuk absen ganjil
+// membuat output genap untuk absen genap
+// Midori Harahap
+
+for(var i=1; i<=10; i++)
+    if(i%2===1){
+        console.log(i);
+    }
---
+++ tugaswhile.js
@@ -0,0 +1,8 @@
+// While menampilkan Angka Genap
+// Midori Harahap
+
+let i = 1;
+while (i < 10){
+    console.log(i);
+    i = i + 2;
+}
