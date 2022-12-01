ec64a249a9ab18f67cd3dc6cdb5b5567c408f905
Midori Harahap <118705697+midoistory@users.noreply.github.com>
2022-12-01 08:59:48 +0700

    question

--- kelompok16.js
+++ kelompok16.js
@@ -2,3 +2,7 @@
 // Kelompok 16 Kalkulator
 // Midori Harahap
 // Zakki Muadzin Abdul Rozak
+
+const input = require('readline-sync');
+var masukannilai1 = input.question('masukkan nilai 1 = ');
+var masukannilai2 = input.question('masukkan nilai 2 = ');
