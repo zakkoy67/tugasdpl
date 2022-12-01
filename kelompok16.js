cb0cc305642ab5bd42fca6e4ec8127e660fdc58b
zakkoy67 <118705766+zakkoy67@users.noreply.github.com>
2022-12-01 09:15:45 +0700

    Delete kelompok16.js

--- kelompok16.js
+++
@@ -1,8 +0,0 @@
-// membuat kalkulator dengan operasi + - * / %
-// Kelompok 16 Kalkulator
-// Midori Harahap
-// Zakki Muadzin Abdul Rozak
-
-const input = require('readline-sync');
-var masukannilai1 = input.question('masukkan nilai 1 = ');
-var masukannilai2 = input.question('masukkan nilai 2 = ');
